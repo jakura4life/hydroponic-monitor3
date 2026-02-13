3423a53e1d26bf0cb8cc0baa92b14068ae1c7ff3
jakura4life <aliff2.azizulazily@live.uwe.ac.uk>
2026-02-13 08:58:25 +0000

    Remove unused services and views

--- web/data/views.py
+++ web/data/views.py
@@ -4,3 +4,32 @@ from .models import hourly_ph_model
 def hourly_ph_view(request):
     data = hourly_ph_model()
     return JsonResponse(data, safe=False)
+
+
+
+# old
+from django.shortcuts import render
+from django.http import JsonResponse
+from services.firebase.firebase_service import get_latest_reading, get_arduino_history_reading
+from services.redis.redis_cache import get_data
+import json
+
+def latest_sensor_data(request):
+    """display firebase db latest directory"""
+    data = get_latest_reading()
+    print(type(data), data)
+    return JsonResponse(data)
+
+def arduino_history_data(request):
+    """display firebase db history directory"""
+    data = get_arduino_history_reading()
+    print(type(data), data)
+    return JsonResponse(data)
+
+def latest_redis_cache(request):
+    """display redis directory"""
+    data = get_data()
+    if data:
+        return JsonResponse(json.loads(data))
+    return JsonResponse({"error": "No data yet"}, status=404)
+
