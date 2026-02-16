b519f45efebbdf2161bcd69964f8583e0d92ee6b
jakura4life <aliff2.azizulazily@live.uwe.ac.uk>
2026-02-16 20:28:45 +0000

    home page backup

--- web/dashboard/urls.py
+++ web/dashboard/urls.py
@@ -3,6 +3,10 @@ from dashboard import views
 
 urlpatterns = [
     path("hourly_history/", views.hourly_view, name="hourly_page"),
+    path("api/hourly_history/", views.hourly_data_api, name="hourly_api"),
     path("current/", views.current_view , name= "current_page"),
     path("api/current/", views.current_data_api , name= "current_api"),
+    path("base/", views.base_view,),
+    path("", views.home_view),
+    
     ]
--- web/dashboard/views.py
+++ web/dashboard/views.py
@@ -3,11 +3,26 @@ from django.shortcuts import render
 from sensor.services.hourly_service import get_hourly_data
 from sensor.services.listen_service import get_latest_reading
 
+def home_view(request):
+    context = {
+        "hourly_api": "api/hourly_history/"
+    }
+    return render(request, "home.html", context)
+
+
+
 def hourly_view(request):
     hourly_data = get_hourly_data(ignore_cache=False)
     return JsonResponse([h.model_dump() for h in hourly_data], safe=False)
 
+def hourly_data_api(request):
+    data = get_hourly_data()
+    if not data:
+        return JsonResponse({"error": "No data yet"}, status=404)
 
+    payload = [h.model_dump(mode="json") for h in data]
+
+    return JsonResponse(payload, safe=False)
 
 def current_view(request):
     return render(request, "dashboard/current.html")
@@ -25,3 +40,7 @@ def current_data_api(request):
         "humidity": reading.humidity,
         "timestamp": reading.datetime.isoformat(),
     })
+
+
+def base_view(request):
+    return render(request, 'base.html')
